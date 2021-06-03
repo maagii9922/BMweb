824f6324b11f59cc0b798ec73132360cd4ead23f
mandah <mandakh@bumanit.mn>
2021-06-03 17:06:12 +0800

    thu

--- BMweb/models.py
+++ BMweb/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class Hereglegch(models.Model):
-    name=models.CharField(max_length=30)
-    code=models.CharField(max_length=30)
+    name=models.CharField(max_length=30, verbose_name=_("Хэрэглэгчийн нэр"))
+    code=models.CharField(max_length=30, verbose_name=_("Хэрэглэгчийн нэр"))
+
+    class Meta:
+        verbose_name = _("Хэрэглэгч")
+        verbose_name_plural = _("Хэрэглэгч")
+
+    def __str__(self):
+        return self.name
